4eda6860e686f2183a05c272ac411f57f2844e80
katyaryabova <keityfly1992@gmail.com>
2019-03-18 00:40:20 +0300

    Code

---
+++ src/Jira.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Collections;
+import java.util.stream.IntStream;
+import java.util.Arrays;
+
+    public abstract class Jira {
+        public static void main(String args[]) {
+            int  myArray[] = {1,2,3};
+            System.out.println(Arrays.stream(myArray).sum());
+
+            // int total = IntStream.of(myArray).sum();
+            //System.out.println(total);
+            System.out.println(Arrays.stream(myArray).sum());
+
+            //Date date = new Date();
+            // System.out.println(date.toString());
+        }
+
+
+
+    }
+
